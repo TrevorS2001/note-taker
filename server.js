54df7fcea2e5f26e4cd0d95bfb32e72bcd792b75
Trevor Snell <trevorsnell@optonline.net>
2022-05-28 11:42:07 -0400

    second

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require("express");
 const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 const { notes } = require("./db/db");
@@ -10,27 +11,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//Generate Unique ID
-const generateUniqueId = require('generate-unique-id');
+//Unique ID
+const uniqueId = require('generate-unique-id');
 
-//Create New Note
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-  };  
+//New Note
+function newNote(body, noteArr) {
+  const note = body;
+  noteArr.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: noteArr }, null, 2)
+  );
+  return note;
+};  
 
 //Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -38,22 +39,26 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  req.body.id = generateUniqueId();
-  const note = createNewNote(req.body, notes);
+  req.body.id = uniqueId();
+  const note = newNote(req.body, notes);
   res.json(note);
 });
     
-
+//Bonus
 app.delete('/api/notes/:id', (req, res) => {
   const { id } = req.params;
 
-  const delNote = notes.findIndex(note => note.id ==id);
+  const deleteNote = notes.findIndex(note => note.id === id);
 
-  notes.splice(delNote, 1);
+  notes.splice(deleteNote, 1);
   return res.send();
 });
 
-//Listener
+//New listener
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
-  });
+  console.log(`App listening on PORT ${PORT}`);
+});
+
+
+
+
